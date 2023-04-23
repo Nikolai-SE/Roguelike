f7e9abaefa5ce2a814c377b618ebca1bf3e48d02
Anton Surkis <antonsurkis@gmail.com>
2023-04-23 14:21:37 +0300

    Add basic tests

---
+++ src/Commons.spec.ts
@@ -0,0 +1,32 @@
+import { ok } from "assert";
+import { add, sub, mul, div, eq } from "./Commons";
+
+describe('Vector', () => {
+        it('should be equal to itself', () => {
+                let v = { x: 1, y: -1 };
+                let u = { x: 1, y: -1 };
+                ok(eq(v, u));
+        });
+        it('should support addition', () => {
+                let v = { x: 1, y: -1 };
+                let u = { x: -1, y: 1 };
+                let vu = { x: 0, y: 0 };
+                ok(eq(vu, add(v, u)));
+        });
+        it('should support subtraction', () => {
+                let v = { x: 1, y: -1 };
+                let u = { x: 1, y: -1 };
+                let vu = { x: 0, y: 0 };
+                ok(eq(vu, sub(v, u)));
+        });
+        it('should support multiplication', () => {
+                let v = { x: 1, y: -1 };
+                let kv = { x: 2, y: -2 };
+                ok(eq(kv, mul(2, v)));
+        });
+        it('should support division', () => {
+                let v = { x: 1, y: -1 };
+                let vu = { x: 0.5, y: -0.5 };
+                ok(eq(vu, div(v, 2)));
+        });
+});
